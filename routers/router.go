eacf8ade5e1f7c61ad357de70f89d450af9229d0
Bernard Mak <b2mak2@gmail.com>
2022-04-05 10:53:58 -0400

    Getting beego setup started

---
+++ routers/router.go
@@ -0,0 +1,11 @@
+package routers
+
+import (
+	"b2mak/theScoreAssignemnt/controllers"
+
+	beego "github.com/beego/beego/v2/server/web"
+)
+
+func init() {
+	beego.Router("/", &controllers.MainController{})
+}
